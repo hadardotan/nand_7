650838d81e5769624054793f0407bf80482d726c
mikabarlev <mikabarlev@gmail.com>
2017-11-25 18:57:40 +0200

    Merge branch 'master' of https://github.com/hadardotan/nand_7
    # Conflicts:
    #	Parser.py
    #	VMtranslator.py

--- Parser.py
+++ Parser.py
@@ -1,26 +1,31 @@
-import os, re
 
-def open_path(path):
+def path_to_lines(file_path):
     """
-    :param path:
-    :return:
+    This function returns a list of string, each string is a line from vm file.
+    :param file_path
+    :return: vm_lines
     """
-    files = {}
-    if os.path.isdir(path):
-        dir = os.listdir(path)
-        for ob in dir:
-            if (not os.path.isdir(ob)) and (
-            os.path.basename(ob).endswith(".vm")):
-                name = os.path.basename(ob)
-                vm_lines = read_vm_file(path + "\\" + name)
-                files[name] = vm_lines
-        return files, path
-    else:
-        name = os.path.basename(path)
-        vm_lines = read_vm_file(path)
-        files[name] = vm_lines
-    return files, os.path.dirname(path)
+    file = open(file_path)
+    lines = []
+    file_length = file_number_of_lines(file_path)
+    for i in range(file_length):
+        line = file.readline()
+        lines.append(line)
+    file.close()
+    return clean_lines(lines)
 
+def file_number_of_lines(file_name):
+    """
+    This function return the lenght (of rows) of vm file
+    :param file_name:
+    :return: line_number
+    """
+    vm_file = open(file_name)
+    line_number = 0
+    while vm_file.readline():
+        line_number += 1
+    vm_file.close()
+    return line_number
 
 def clean_lines(vm_lines):
     """
@@ -34,46 +39,14 @@ def clean_lines(vm_lines):
         comment_start = line.find("//")
         if line == "" or comment_start == 0:
             continue
-        elif comment_start >0:
+        elif comment_start > 0:
             line = line[:comment_start]
         new_lines.append(line)
     return new_lines
 
-def get_asm_file_name(file_path):
-    """
-
-    :param file_path:
-    :return:
-    """
-    file_name = re.sub(r'(\.vm)$', '', file_path)
-    asm_file_name = file_name + ".asm"
-    return asm_file_name
-
-def read_vm_file(file_path):
-    """
-    This function returns a list of string, each string is a line from vm file.
-    :param file_path
-    :return: vm_lines
-    """
-    vm_file = open(file_path)
-    vm_lines = []
-    file_length = number_of_lines(file_path)
-    for i in range(file_length):
-        line = vm_file.readline()
-        vm_lines.append(line)
-    vm_file.close()
-    vm_lines = clean_lines(vm_lines)
-    return vm_lines
-
-def number_of_lines(file_name):
-    """
-    This function return the lenght (of rows) of vm file
-    :param file_name:
-    :return: line_number
-    """
-    vm_file = open(file_name)
-    line_number = 0
-    while vm_file.readline():
-        line_number += 1
-    vm_file.close()
-    return line_number
+def line_lst_2_str(line):
+    str = ""
+    for l in line:
+        str += l
+        str += "\n"
+    return str
--- file_object.py
+++ file_object.py
@@ -1,34 +1,292 @@
+import re
+import Parser
+
+Sp = "SP"
+Local = "LCL"
+Argument = "ARG"
+This = "THIS"
+That = "THAT"
+Temp = "5"
+Static = "16"
+
+class File(object):
+    """
+    handles the parsing of single .vm file
+    reads vm commands, parse it into lexical components and provides convenient
+    access to these components
+    ingnores all whitespaces and comments
+    """
+    def __init__(self, file_path):
+        """
+        opens the input file and gets ready to parse it
+        :param input_file:
+        """
+        self.vm_path = file_path
+        self.vm_lines = Parser.path_to_lines(self.vm_path)
+        self.asm_path = re.sub(r'(\.vm)$', '', self.vm_path) + ".asm"
+        self.file_name = (re.split('(\w+)', self.vm_path))[-4]
+        self.counter = 0
+
+    def translate(self):
+        asm_file = open(self.asm_path, "w+")
+        lines = self.vm_lines
+        for i in range(len(lines)):
+            translated = self.vm_to_asm(lines[i])
+            asm_file.write(translated)
+        asm_file.close()
+
+    def vm_to_asm(self, line):
+        """
+        translates vm line to asm code
+        :param file: file object
+        :param i: index of line in file to translate
+        :return:
+        """
+        as_list = line.split(' ')
+        if (len(as_list)) == 1:
+            return self.aritmetics_commands(line.strip())
+        command, segment, i = as_list[0], as_list[1], as_list[2]
+        if segment == 'this' or segment == 'that':
+            return self.this_that_command(command,segment,i)
+        if segment == 'constant':
+            return self.constant_command(i)
+        if segment == "local" or segment == "argument":
+            return self.local_argument_command(command, segment, i)
+        if segment == 'temp':
+            return self.temp_command(command, i)
+        if segment == 'pointer':
+            return self.pointer_command(command, i)
+        if segment == 'static':
+            return self.static_command(command, i)
 
 
-# Constants
+    def this_that_command(self, command , segment , i):
+        line = ["// " + command + " " + segment + " " + i]
+        #line =[]
+        ram = That
+        if segment == 'this':
+            ram = This
+        line.append("@" + ram)
+        line.append("D=M")
+        line.append("@"+i)
+        if command == 'push':
+            line.append("A=D+A")
+            line.append("D=M")
+            line.append("@SP")
+            line.append("A=M")
+            line.append("M=D")
+            line.append("@SP")
+            line.append("M=M+1")
+            return Parser.line_lst_2_str(line)
+        if command == 'pop':
+            line.append("D=D+A")
+            line.append("@R13")
+            line.append("M=D")
+            line.append("@SP")
+            line.append("AM=M-1")
+            line.append("D=M")
+            line.append("@R13")
+            line.append("A=M")
+            line.append("M=D")
+            return Parser.line_lst_2_str(line)
 
-#Commands
-C_ARITHMETIC = 0
-C_PUSH = 1
-C_POP = 2
-C_LABEL = 3
-C_GOTO = 4
-C_IF = 5
-C_FUNCTION = 6
-C_RETURN = 7
-C_CALL = 8
+    def constant_command(self,i):
+        """
+        generates asm code for constant vm command
+        :param i:
+        :return: string  of asm code for constant vm command
+        """
+        line = ["// push constant " + i]
+        line.append("@" + i)
+        line.append("D=A")
+        line.append("@SP")
+        line.append("A=M")
+        line.append("M=D")
+        line.append("@SP")
+        line.append("M=M+1")
+        return Parser.line_lst_2_str(line)
 
-#Segments and Symbols
-LCL = "local"
-ARG = "argument"
-THIS = "this"
-THAT = "that"
-TEMP = "temp"
-POINTER = "pointer"
-STATIC = "static"
-CONST = "constant"
+    def local_argument_command(self, command , segment , i):
+        """
+        generates asm code for segment vm command
+        :param file: file object
+        :param command:
+        :param segment:
+        :param i:
+        :return:
+        """
+        line = ["// " + command + " " + segment + " " + i]
+        line.append("@" + self.ram_for_segment(segment))
+        line.append("D=M")
+        line.append("@" + i)
+        if command == 'push':
+            line.append("A=D+A")
+            line.append("D=M")
+            line.append("@SP")
+            line.append("A=M")
+            line.append("M=D")
+            line.append("@SP")
+            line.append("M=M+1")
+            return Parser.line_lst_2_str(line)
+        if command == 'pop':
+            line.append("D=D+A")
+            line.append("@R13")
+            line.append("M=D")
+            line.append("@SP")
+            line.append("AM=M-1")
+            line.append("D=M")
+            line.append("@R13")
+            line.append("A=M")
+            line.append("M=D")
+            return Parser.line_lst_2_str(line)
+
+    def temp_command(self, command , i):
+        line = ["// " + command + " temp " + i]
+        line.append("@" + self.ram_for_segment("temp"))  ### R5 for temp?
+        line.append("D=M")
+        index = int(self.ram_for_segment("temp")) + int(i)
+        line.append("@" + str(index))      # D = 5 + i
+        if command == 'push':
+            line.append("A=D+A")
+            line.append("D=M")
+            line.append("@SP")
+            line.append("A=M")
+            line.append("M=D")
+            line.append("@SP")
+            line.append("M=M+1")
+            return Parser.line_lst_2_str(line)
+        if command == 'pop':
+            line.append("D=D+A")
+            line.append("@R13")
+            line.append("M=D")
+            line.append("@SP")
+            line.append("AM=M-1")
+            line.append("D=M")
+            line.append("@R13")
+            line.append("A=M")
+            line.append("M=D")
+            return Parser.line_lst_2_str(line)
+
+    def pointer_command(self, command, i):
+        ram = That
+        if i == "0":
+            ram = This
+        line = ["// " + command + " pointer " + i]
+        line.append("@" + ram)
+        if command == 'push':
+            line.append("D=M")
+            line.append("@SP")
+            line.append("A=M")
+            line.append("M=D")
+            line.append("@SP")
+            line.append("M=M+1")
+            return Parser.line_lst_2_str(line)
+        if command == 'pop':
+            line.append("D=A")
+            line.append("@R13")
+            line.append("M=D")
+            line.append("@SP")
+            line.append("AM=M-1")
+            line.append("D=M")
+            line.append("@R13")
+            line.append("A=M")
+            line.append("M=D")
+            return Parser.line_lst_2_str(line)
+
+    def static_command(self,command, i):
+        line = ["//" + command + "static" + i]
+        name = self.file_name + "." + i
+        index = 16 + int(i)
+        line.append("@" + str(index))
+        if command == 'pop':
+            line.append("D=A")
+            line.append("@R13")
+            line.append("M=D")
+            line.append("@SP")
+            line.append("AM=M-1")
+            line.append("D=M")
+            line.append("@R13")
+            line.append("A=M")
+            line.append("M=D")
+            return Parser.line_lst_2_str(line)
+        if command == 'push':
+            line.append("D=M")
+            line.append("@SP")
+            line.append("A=M")
+            line.append("M=D")
+            line.append("@SP")
+            line.append("M=M+1")
+            return Parser.line_lst_2_str(line)
+
+    def aritmetics_commands(self, command):
+        """
+        :param command:
+        :return:
+        """
+        line = ["// " + command]
+        if command == "neg" or command == "not":
+            line.append("@SP")
+            line.append("A=M")
+            if command == "neg":
+                line.append("D=-M")
+            else:
+                line.append("D=!M")
+            line.append("M=D")
+            return Parser.line_lst_2_str(line)
+        if command == "eq":
+            return self.eq_command()
+        if command == "gt" or command == "lt": ####
+            return ""
+        line.append("@SP")
+        line.append("AM=M-1")
+        line.append("D=M")
+        line.append("A=A-1")
+        if command == "add":
+            line.append("M=M+D")
+        if command == "sub":
+            line.append("M=M-D")
+        if command == "and":
+            line.append("M=D&M")
+        if command == "or":
+            line.append("M=D|M")
+        return Parser.line_lst_2_str(line)
+
+    def ram_for_segment(self, segment):
+        if segment == 'temp':
+            return Temp
+        if segment == 'local':
+            return Local
+        if segment == 'argument':
+            return Argument
+        if segment == 'this':
+            return This
+        if segment == 'that':
+            return That
+        if segment == 'static':
+            return Static
 
 
-
-
-
-
-
+    def eq_command(self):
+        line = ["// eq"]
+        line.append("@SP")
+        line.append("AM=M-1")
+        line.append("D=M")
+        line.append("A=A-1")
+        line.append("D=M-D")
+        line.append("@FALSE"+str(self.counter))
+        line.append("D;JNE")
+        line.append("@SP")
+        line.append("A=M-1")
+        line.append("M=-1")
+        line.append("@CONTINUE"+str(self.counter))
+        line.append("0;JMP")
+        line.append("(FALSE"+str(self.counter)+")")
+        line.append("@SP")
+        line.append("A=M-1")
+        line.append("M=0")
+        line.append("(CONTINUE"+str(self.counter)+")")
+        self.counter +=1
+        return Parser.line_lst_2_str(line)
 
 
 
